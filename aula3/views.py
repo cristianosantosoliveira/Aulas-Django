b3b456324c09da71fca7a98d90dbbefb3fb1cc94
cristianosantosoliveira <cristiano.santos.oliveira@outlook.com>
2020-02-15 16:05:06 -0300

    Recuperando dados da requisição

--- aula3/views.py
+++ aula3/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -15,3 +16,40 @@ def setacookie(request):
 
 def redireciona(request):
     return HttpResponseRedirect("https://www.uol.com.br")
+
+def show_code(request, code):
+     html = f"<h1>The code is: { code}</h1>"
+     response = HttpResponse(html)
+     return response
+
+def cat_status(request, code):
+     HttpResponseRedirect(f'https://http.cat/{code}')
+
+
+def show_get_values(request):
+    nome = request.GET.get("Nome: ", None)
+    if nome is None:
+        html = f"<h1>Bem vindo ao usuário anomino </h1>"
+    else:
+        html = f"<h1>Bem vindo ao usuário {nome}</h1>"
+    return HttpResponse(html)
+
+@csrf_exempt
+def show_post_values(request):
+    head = ""
+    if request.method == "POST":
+        nome =  request.POST.get("nome")
+        sobrenome = request.POST.get("sobrenome")
+        head += f"<h1> bem vindo {nome} {sobrenome} </h1>"
+    html = """
+    <form method=POST>
+    <label for="nome">First name:</label><br>
+    <input type="text" id="nome" name="nome" value=""><br>
+    <label for="sobrenome">Sobrenome:</label><br>
+    <input type="text" id="sobrenome" name="sobrenome" value=""><br><br>
+    <input type="submit" value="Submit">
+    </form>
+"""
+    htl_to_response = head + html
+    return HttpResponse(htl_to_response)
+
